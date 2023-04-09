f2dc15b3f76dfaed2586d9a7e563a24e6fb4cdf1
DimitrusB <dimitrusboss@mail.ru>
2023-04-09 23:10:32 +0300

    cikl

---
+++ script.js
@@ -0,0 +1,155 @@
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// // for (let elem of arr)
+// // console.log(elem);
+// ============================================
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for (let index = 0; index < arr.length; index++) {  
+//     console.log(arr[index]);
+// }
+// ============================================
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// for (let index = 1; index < arr.length; index++){
+//     console.log(arr[index]);
+//   }
+  // ============================================
+//   let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//   arr= arr.reverse();
+//   console.log(arr);
+  // ============================================
+//   let arr = ['a', 'b', 'c', 'd', 'e'];
+		
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]);
+// }
+// ============================================
+// let arr = [2, 5, 9, 15, 1, 4];
+// for (let index  of arr) {
+//     if (index % 2 === 1) {
+//         console.log(index)
+//     }
+    
+// }
+// ============================================
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+// for (let key in obj){
+//     if (obj[key] % 2 === 0){
+//         console.log(obj[key]);
+//     }
+// }
+// ============================================
+// let i=1;
+// while (i<=100) {
+//     console.log(i);
+//     i++;
+// }
+// ============================================
+// let i=11;
+// while (i>=11 && i<=33) {
+//     console.log(i);
+//     i++;  
+// }
+// ============================================
+// let num=25;
+// j=0;
+// while (num <=1000) {
+//     j=j+1;
+//     num=num*3;
+    
+    
+// }
+// console.log(j , num);
+// ============================================
+// let res =0;
+// for (let index = 2; index <= 100; index++) {
+//     if (index %2 ===0){
+//     res = res+index;
+// }
+//     }
+// console.log(res);
+// ============================================
+// let res =0;
+// for (let index = 1; index < 99; index++) {
+//     if (index % 2 === 1){
+//     res +=index;
+//     }
+// }
+// console.log(res);
+// ============================================
+// let res =1;
+// for (let index = 1; index <= 20; index++) {
+//     res *=index;
+// }
+// console.log(res);
+// ============================================
+// let arr = [2, 5, 9, 15, 1, 4];
+// for (let elem of arr) {
+//   if (elem>3 && elem<10) {
+//     console.log(elem);
+//   }
+// }
+// ============================================
+// let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+// for (let key in obj){
+//     if (obj[key] % 2 === 0){
+//         console.log(obj[key]);
+//     }
+// }
+// ============================================
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res=0;
+// for (let el of arr) {
+//   res +=el;
+// }
+//   console.log(res);
+// ============================================
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res=0;
+// for (let el of arr) {
+//   if(el %2 === 0){
+//     res+=el;
+//   }
+// }
+// console.log(res);
+// // ============================================
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 0;
+// for (let el  of arr) {
+//   res += Math.pow(el, 2);
+// }
+// console.log(res);
+// ============================================
+// let arr = [2, 5, 9, 3, 1, 4];
+// let res = 1;
+// for (let el of arr) {
+//   res *=el;
+// }
+// console.log(res);
+// ============================================
+// let str = '';
+// for (let index = 0; index < 5; index++) {
+//   str += '-';
+// }
+// console.log(str);
+// ============================================
+// let str = '';
+// for (let index = 1; index <=9 ; index++) {
+//   str +=index;
+  
+// }
+// console.log(str);
+// ============================================
+// let str = '';
+// for (let index = 9; index >=1 ; index--) {
+//   str +=index;
+  
+// }
+// console.log(str);
+// ============================================
+// let str = '-';
+// for (let index = 1; index <=9 ; index++) {
+//   str +=index;
+//   str +='-';
+// }
+// console.log(str);
+
